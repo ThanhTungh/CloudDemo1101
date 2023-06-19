453b5e610f38d0c295a1a77f02aa64a7b212622f
newbiewer <92965393+ThanhTungh@users.noreply.github.com>
2023-06-19 16:44:22 +0700

    remove style.css for better view

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -4,7 +4,10 @@ const ProductSchema = mongoose.Schema
     name: String,
     price: String,
     pictureURL: String,
-    brand: String
+    brand: String,
+    video: String,
+    category:String
+
 });
 const ProductsModel = mongoose.model("products", ProductSchema, "products");
 module.exports = ProductsModel;
