71c78d829eac8006e2decbd1cc2914bc1f1a30a2
newbiewer <92965393+ThanhTungh@users.noreply.github.com>
2023-06-19 10:05:06 +0700

    .

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,6 @@ router.post('/check', async(req, res) => {
     if (username === 'admin' && password === '123456') {
         res.render('home', { title: 'Express' });
     } else {
-        // res.send("<h1 style='color: red; background-color: yellow';>Login failed!</h1> ");
         loginFailed = true;
         res.render('login', { loginFailed });
     }
